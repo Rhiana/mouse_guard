fca3dca6fd3a66a38de5b1f90cc62ef631f7d45e
Rhiana Heath <rhiana@heath.cc>
2014-02-16 12:55:05 +1100

    map kinda working yay

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,6 +1,6 @@
-#Leaflet.tile_layer = "/mouse_guard_tiles/{z}/{x}/{y}.png"
 # You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
 
-Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/YOUR-CLOUDMADE-API-KEY/997/256/{z}/{x}/{y}.png"
+#Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/YOUR-CLOUDMADE-API-KEY/997/256/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "/mouse_guard_tiles/{z}/{x}/{y}.png"
 Leaflet.attribution = "Jykke ref: http://www.cartographersguild.com/finished-maps/11075-mouse-guard-map-facelift.html"
-Leaflet.max_zoom = 18
+Leaflet.max_zoom = 4
