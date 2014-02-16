d216fcf5f36a7249d7b6a5650b7552316942a518
Rhiana Heath <rhiana@heath.cc>
2014-02-16 15:16:41 +1100

    fixed map view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,19 +19,21 @@ $(function () {
 
 	var map = L.map('map')
 
-	map.setView([79.06765, -88.06641], 3)
+	//marker_lh = L.marker([73.76964, -65.16016]).addTo(map)
+	//marker_lh.bindPopup('Lockhaven')
 
-	marker_lh = L.marker([73.76964, -65.16016]).addTo(map)
-	marker_lh.bindPopup('Lockhaven')
-
-	marker_br = L.marker([71.62907, -47.10937]).addTo(map)
-	marker_br.bindPopup('Blackrock')
+	//marker_br = L.marker([71.62907, -47.10937]).addTo(map)
+	//marker_br.bindPopup('Blackrock')
 
 
 	L.tileLayer('/mouse_guard_tiles/{z}/{y}/{x}.png', {
 	          attribution: 'Jykke ref: http://www.cartographersguild.com/finished-maps/11075-mouse-guard-map-facelift.html',
-	          maxZoom: 4
+	          maxZoom: 4,
+	          minZoom: 2
 	      }).addTo(map)
+
+	map.setView([0, 0], 2)
+	map.setMaxBounds(map.getBounds())
 
 	function onMapClick(e) {
 	    alert("You clicked the map at " + e.latlng);
