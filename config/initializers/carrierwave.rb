40bc51a918eda03e56eba00caadf4531291bb548
Rhiana Heath <rhiana@heath.cc>
2014-03-20 21:05:30 +1100

    Bugfixed js and css. Commented out uploading files. Bugfixed config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => 'ap-southeast-2',
-    :endpoint               => 'https://s3-ap-southeast-2.amazonaws.com'
-  }
-  config.fog_directory  = 'mouseguard-uploads'
-  config.fog_public     = false
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+if ENV['AWS_SECRET_ACCESS_KEY']
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => 'ap-southeast-2',
+      :endpoint               => 'https://s3-ap-southeast-2.amazonaws.com'
+    }
+    config.fog_directory  = 'mouseguard-uploads'
+    config.fog_public     = false
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+  end
 end
-
 # http://mouseguard-uploads.s3-website-ap-southeast-2.amazonaws.com/
